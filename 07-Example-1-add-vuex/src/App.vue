036922229380010b12ae8f6338004c24462069a4
Raj Bhatta <raj@sstwireless.com>
2021-05-10 21:31:37 -0700

    Adding vuex dependency to the project

---
+++ 07-Example-1-add-vuex/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <product-list-one v-bind:products="products"></product-list-one>
+    <product-list-two v-bind:products="products"></product-list-two>
+  </div>
+</template>
+
+<script>
+import ProductListOne from './components/ProductListOne.vue'
+import ProductListTwo from './components/ProductListTwo.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      products: [
+        {name:'Apple', qtn:30},
+        {name:'Ball', qtn:30},
+         {name:'Cat', qtn:30},
+         {name:'Dog', qtn:30}
+         ]
+    }
+  },
+  components: {
+    ProductListOne,
+    ProductListTwo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
